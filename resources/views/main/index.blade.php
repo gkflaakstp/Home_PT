b258b2bc8095ec2260b9aea07b36ba3c7dc791c8
Halim5959 <gkflaakstp12@gmail.com>
2022-09-21 16:56:59 +0900

    공통파일 생성

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
 <html lang="ko">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/public.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-        <script src="{{ asset('/js/main.js') }}"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-        <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-        <title>홈피티</title>
+        @include('public')
+        <title></title>
     </head>
     <body>
         <section class="wrap">
