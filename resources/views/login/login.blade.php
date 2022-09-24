66795b2181f3b96f6cad80da599c815356831dea
name <gkflaakstp12@gmail.com>
2022-09-24 20:53:40 +0900

    로그인

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="default.css">
+    <link rel="stylesheet" href="style.css">
+    <title>HOMEPT 로그인</title>
+</head>
+<body>
+    <section class="login">
+        <h2 href="">HOMEPT</h2>
+        <ul>
+            <li><input type="text" placeholder="아이디" title="아이디입력"></li>
+            <li><input type="password" placeholder="비밀번호" title="비밀번호입력"></li>
+            <li><input type="checkbox" id="chk_id"><label for="chk_id">아이디저장</label></li>
+            <li><button>로그인</button></li>
+        </ul>
+        <div>
+            <ul>
+            <li><a href="../membership/membership.html">회원가입</a></li>
+            <li><a href="">아이디 찾기</a></li>
+            <li><a href="">비밀번호 찾기</a></li>
+            </ul>
+        </div>
+    </section>
+    </section>
+</body>
+</html>
